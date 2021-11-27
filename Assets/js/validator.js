6952bd8295d912e8164223c6b0f64b8313638d30
Do Tien Dat <datblu2003@gmail.com>
2021-11-28 01:13:06 +0700

    add

--- Assets/js/validator.js
+++ Assets/js/validator.js
@@ -1,20 +1,20 @@
-function Validator(formSelector){
+function Validator(formSelector) {
     var formRules = {};
     var validatorRules = {
-        required: function(value){
+        required: function(value) {
             return value.trim() ? undefined : 'Vui lòng nhập trường này';
         },
-        email: function(value){
+        email: function(value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
             return regex.test(value) ? undefined : 'Trường này phải là email';
         },
-        min: function(min){
-            return function(value){
+        min: function(min) {
+            return function(value) {
                 return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} ký tự`;
             }
         },
-        max: function(max){
-            return function(value){
+        max: function(max) {
+            return function(value) {
                 return value.length <= max ? undefined : `Vui lòng nhập tối đa ${min} ký tự`;
             }
         },
@@ -22,12 +22,16 @@ function Validator(formSelector){
 
     var formElement = document.querySelector(formSelector);
 
-    if(formElement){
+    if (formElement) {
         var inputs = formElement.querySelectorAll('[name][rules]');
-        for(var input of inputs){
+        for (var input of inputs) {
 
             var rules = input.getAttribute('rules').split('|');
-            for(var rule of rules){
+            for (var rule of rules) {
+                if (rule.includes(':')) {
+                    var ruleInfo = rule.split(':')
+                    console.log(ruleInfo);
+                }
                 console.log(rule);
             }
 
